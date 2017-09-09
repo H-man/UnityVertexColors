fc94b3ed0e8625ba1cca1664759f696c9f66391e
Henri Kähkönen <henri@fattail.fi>
2017-09-09 23:42:43 +0300

    Updated to unity 2017.1.1f1

---
+++ Assets/Clicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+
+  Camera _cam;
+
+  public LayerMask HitLayer;
+  void Start()
+  {
+    _cam = Camera.main;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      RaycastHit hit;
+      Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(ray, out hit, 100.0f, HitLayer))
+      {
+        var rotator = hit.transform.GetComponent<SimpleRotator>();
+        if (rotator.Speed != Vector3.zero)
+        {
+          rotator.Speed = Vector3.zero;
+        }
+        else
+        {
+          rotator.Speed = Random.insideUnitSphere;
+        }
+
+        Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+      }
+    }
+
+  }
+}
---
+++ Assets/SimpleRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotator : MonoBehaviour
+{
+
+  public Vector3 Speed = new Vector3(0, 1, 0);
+
+
+  void Update()
+  {
+    transform.RotateAround(transform.position, Speed, 10);
+  }
+}
